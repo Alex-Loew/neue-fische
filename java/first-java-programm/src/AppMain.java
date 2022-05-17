29a9e664ba542f1ac37fd24cbba4b9b2f09f2c10
Alex Löw <alexloew@hotmail.de>
2022-05-17 15:58:37 +0200

    add new operators to program

--- java/first-java-programm/src/AppMain.java
+++ java/first-java-programm/src/AppMain.java
@@ -1,5 +1,21 @@
 public class AppMain {
     public static void main(String[] args) {
         System.out.println("Hallo an alle!");
+
+        int Num1 = 1;
+        int Num2 = 2;
+
+        var Summe = Num1 + Num2;
+
+        System.out.println(Summe);
+
+        System.out.println("Summe: "+Summe);
+
+        boolean result = Num1 > Num2;
+        System.out.println(Num1 + " ist größer als " + Num2 + ": " + result);
+        result = Num1 < Num2;
+        System.out.println(Num1 + " ist kleiner als " + Num2 + ": " + result);
+        result = Num1 == Num2;
+        System.out.println(Num1 + " ist gleich " + Num2 + ": " + result);
     }
 }
